37ddfcfc1c7318a51e80829fec518720bb966715
Vegard Bjørsvik <vegardbjr@gmail.com>
2015-09-27 11:52:58 +0200

    Now checks for right file type

--- app/routes/api.js
+++ app/routes/api.js
@@ -16,16 +16,28 @@ var storage = multer.diskStorage({
 	}
 });
 
-var upload = multer({ storage: storage });
+function fileFilter(req, file, cb) {
+	if (file.mimetype == 'audio/mp3') {
+		cb(null, true)
+	} else {
+		cb(null, false);
+	}
+}
+
+var upload = multer({ storage: storage , fileFilter: fileFilter});
 
 module.exports = function(app, express) {
 	var apiRoutes = express.Router();
 	apiRoutes.get('/', function(req, res) {
-		res.json({ message: 'Welcome to kfsongs api! Explore the api at https://kfsongs.com/api/'})
+		res.json({ message: 'Welcome to kfsongs! Explore the application at https://kfsongs.com/'})
 	});
 
 	apiRoutes.post('/upload', upload.single('song'), function(req, res, next) {
-		return res.json({ message: "Upload complete!", link: "https://127.0.0.1:8080/s/"+songid});
+		if (req.file) {
+			return res.json({ message: "Upload complete!", link: "https://127.0.0.1:8080/s/"+songid});
+		} else {
+			return res.status(415).send({ error: "Wrong file type! Please use mp3"});
+		}
 		next();
 	});
 
